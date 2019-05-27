f593bf1ba04e967d375c84eba1de4002b92de0b7
Skeledon <Darkgoon@hotmail.it>
2019-05-27 15:07:20 +0200

    second

---
+++ Assets/Scripts/CrosshairMousePositionLink.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairMousePositionLink : MonoBehaviour
+{
+    public Camera MainCam;
+
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        t.position = MainCam.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10;
+    }
+}
---
+++ Assets/Scripts/SoldierController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Transform t;
+
+    private void Awake()
+    {
+        t = transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector2 dir)
+    {
+        t.Translate(dir * speed * Time.deltaTime);
+    }
+}
