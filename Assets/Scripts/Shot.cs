e9cd29bd59e20fc0f18e318f145e1b915109c643
Skeledon <Darkgoon@hotmail.it>
2019-05-29 14:14:22 +0200

    iniziati i proiettili

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private float LifeTime;
+
+    private GameObject myPool;
+    private Transform t;
+    private float currentTime;
+
+    private void Awake()
+    {
+        t = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.Translate(t.up * Speed * Time.deltaTime);
+        currentTime += Time.deltaTime;
+        if (currentTime >= LifeTime)
+            DestroySelf();
+    }
+
+    public void Init()
+    {
+        
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
